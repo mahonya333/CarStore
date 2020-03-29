78bcc13f22d8565a643b9c6da88958452d6ccbe9
mahonya333 <mahonya333@gmail.com>
2020-03-29 12:01:24 +0300

    сверстан адаптив и слайдер  секции offers

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,23 @@ $(function () {
         autoplaySpeed: 2000,
         variableWidth: true
     });
+    $('.offers-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+          ]
+    });
 
 
 });
