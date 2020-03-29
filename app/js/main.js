d8b34a8e63a71878821b613ae68d51e01fda0355
mahonya333 <mahonya333@gmail.com>
2020-03-29 11:26:55 +0300

    установлены npm jquery, rateYou, jquery, настроен слайдер для секции featured,сверстан адаптив и правки секции featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
-$(function(){
+$(function () {
+
+    $('.featured-slider').slick({
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 2000,
+        variableWidth: true
+    });
 
 
-
-  
 });
